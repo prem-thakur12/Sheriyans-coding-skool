851673e9be195a9e5ab8989f93ae50abdb69c85b
Prem Thakur <tprem0301@gmail.com>
2026-08-07 06:43:20 +0530

    sheriyans coding skool

---
+++ Typescript/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var a = 12;
+a = "harsh";
+
+let arr =[1,2,3,"prem"]
---
+++ Typescript/app.ts
@@ -0,0 +1,31 @@
+// Basic Types
+// -> Primitive types (number, string, boolean)
+// -> Arrays
+// -> Tuples
+// -> Enums
+// -> Any, Unknown, void, Null, Undefined, Never
+
+//primitive and refrence :-
+// [] {} ()      = refrence 
+// var a = 12;   = primitive
+// var b = a;    = primitive
+
+// let a =12;
+// a = "prem"
+
+//Arrays :-
+// let arr = [1,2,3,"ptre"];
+
+//Tuples :-
+let a = 20
+let b = a;
+
+
+
+
+
+
+
+
+
+
